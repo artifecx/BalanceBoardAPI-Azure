fd83c2edd97b6e55c6d1201b1a8ed8c909d4ebc8
artifecx <kebeniga@gmail.com>
2025-06-23 20:17:12 +0800

    Implement Transaction queries

--- Application/Features/Transactions/Queries/GetTransactionByIdQueryHandler.cs
+++ Application/Features/Transactions/Queries/GetTransactionByIdQueryHandler.cs
@@ -2,6 +2,7 @@
 using Application.Common.Interfaces;
 using Application.Common.Mediator;
 using Application.Dtos.Transactions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Transactions
 {
@@ -9,7 +10,31 @@ namespace Application.Features.Transactions
     {
         public async Task<Result<TransactionDto>> Handle(GetTransactionByIdQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var userId = request.UserId;
+            var transactionId = request.Id;
+
+            if (transactionId == Guid.Empty || userId == Guid.Empty)
+                return Result<TransactionDto>.Failure("Unable to process request, missing IDs.", 400);
+
+            var transaction = await context.Transactions
+                .AsNoTracking()
+                .FirstOrDefaultAsync(t => t.Id == transactionId && t.UserId == userId, cancellationToken);
+
+            if (transaction is null)
+                return Result<TransactionDto>.Failure("Transaction not found.", 404);
+
+            var transactionDto = new TransactionDto
+            (
+                Id: transaction.Id,
+                AccountId: transaction.AccountId,
+                CategoryId: transaction.CategoryId ?? Guid.Empty,
+                Amount: transaction.Amount,
+                Note: transaction.Note,
+                TransactionDate: transaction.TransactionDate,
+                Type: transaction.Type
+            ); 
+
+            return Result<TransactionDto>.Success(transactionDto);
         }
     }
 }
--- Application/Features/Transactions/Queries/GetTransactionsQueryHandler.cs
+++ Application/Features/Transactions/Queries/GetTransactionsQueryHandler.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Common;
 using Application.Common.Interfaces;
 using Application.Common.Mediator;
+using Application.Dtos.Accounts;
 using Application.Dtos.Transactions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Transactions
 {
@@ -9,7 +11,30 @@ namespace Application.Features.Transactions
     {
         public async Task<Result<List<TransactionDto>>> Handle(GetTransactionsQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var userId = request.UserId;
+
+            if (userId == Guid.Empty)
+                return Result<List<TransactionDto>>.Failure("Unable to process request, User ID is not provided.", 400);
+
+            var transactions = await context.Transactions
+                .Where(t => t.UserId == userId)
+                .Select(a => new TransactionDto
+                (
+                    a.Id,
+                    a.AccountId,
+                    a.CategoryId ?? Guid.Empty,
+                    a.Amount,
+                    a.Note,
+                    a.TransactionDate,
+                    a.Type
+                ))
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            if (transactions is null || transactions.Count == 0)
+                return Result<List<TransactionDto>>.Failure("No transactions found for the specified user.", 404);
+
+            return Result<List<TransactionDto>>.Success(transactions);
         }
     }
 }

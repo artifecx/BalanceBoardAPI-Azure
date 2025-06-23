eb9168247318c718d4b54dfad263332cc7b82d72
artifecx <kebeniga@gmail.com>
2025-06-23 13:42:59 +0800

    Remove nullable '?' in Dto properties

--- Application/Dtos/Auth/RegisterUserDto.cs
+++ Application/Dtos/Auth/RegisterUserDto.cs
@@ -4,5 +4,5 @@ public class RegisterUserDto
 {
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
-    public string? ConfirmPassword { get; set; } = null!;
+    public string ConfirmPassword { get; set; } = null!;
 }

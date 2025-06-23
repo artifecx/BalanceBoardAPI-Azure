680606b7e6ebb441680fe69a9002da5f222457ad
artifecx <kebeniga@gmail.com>
2025-06-23 13:56:02 +0800

    Add StatusCode property to Result

--- Application/Common/Result.cs
+++ Application/Common/Result.cs
@@ -5,7 +5,8 @@ public class Result<T>
     public bool IsSuccess { get; set; }
     public T? Data { get; set; }
     public string? Error { get; set; }
+    public int? StatusCode { get; set; }
 
     public static Result<T> Success(T data) => new() { IsSuccess = true, Data = data };
-    public static Result<T> Failure(string error) => new() { IsSuccess = false, Error = error };
+    public static Result<T> Failure(string error, int statusCode) => new() { IsSuccess = false, Error = error, StatusCode = statusCode };
 } 
